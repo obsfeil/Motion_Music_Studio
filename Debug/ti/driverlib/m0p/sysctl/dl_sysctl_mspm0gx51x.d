a557290f45f4565ec3882eef20eee953fce0a053
obsfeil <obsfeil@outlook.com>
2025-12-18 15:25:03 +0100

    closer

---
+++ Debug/ti/driverlib/m0p/sysctl/dl_sysctl_mspm0gx51x.d
@@ -0,0 +1,6 @@
+# FIXED
+
+ti/driverlib/m0p/sysctl/dl_sysctl_mspm0gx51x.o: \
+ ../ti/driverlib/m0p/sysctl/dl_sysctl_mspm0gx51x.c \
+ C:/ti/mspm0_sdk_2_08_00_04/source/ti/devices/DeviceFamily.h
+C:/ti/mspm0_sdk_2_08_00_04/source/ti/devices/DeviceFamily.h:
