cd5ef02e7f66d02b1e082b3c227d3a9e935f9ea7
obsfeil <obsfeil@outlook.com>
2025-12-23 08:00:19 +0100

    closer

--- Debug/objects.mk
+++ Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -Wl,-ldriverlib.a -Wl,-llibiqmath_MATHACL.a -Wl,-llibc.a -Wl,-ldevice.cmd.genlibs
+LIBS := -Wl,-l"C:/ti/mspm0_sdk_2_09_00_01/source/ti/driverlib/lib/ticlang/m0p/mspm0g1x0x_g3x0x/driverlib.a" -Wl,-l"C:/ti/mspm0_sdk_2_09_00_01/source/ti/iqmath/lib/ticlang/m0p/mathacl/mspm0g1x0x_g3x0x/iqmath.a" -Wl,-llibc.a
 
