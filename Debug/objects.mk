2c63188a6c9798d3f193487ab1b91187b29507bf
obsfeil <obsfeil@outlook.com>
2025-12-28 10:23:35 +0100

    Rydding: Fjernet kompilerte filer og la til .gitignore

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,-l"C:/ti/mspm0_sdk_2_09_00_01/source/ti/driverlib/lib/ticlang/m0p/mspm0g1x0x_g3x0x/driverlib.a" -Wl,-l"C:/ti/mspm0_sdk_2_09_00_01/source/ti/iqmath/lib/ticlang/m0p/mathacl/mspm0g1x0x_g3x0x/iqmath.a" -Wl,-llibc.a
-
